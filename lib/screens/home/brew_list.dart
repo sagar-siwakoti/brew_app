cdef5cae0d4b9dfe332f4a20beb81bc212cc49cf
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-09-27 23:56:36 +0545

    -added brew model and got list form database

---
+++ lib/screens/home/brew_list.dart
@@ -0,0 +1,25 @@
+import 'package:brew_coffee/models/brew.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class BrewList extends StatefulWidget {
+  @override
+  _BrewListState createState() => _BrewListState();
+}
+
+class _BrewListState extends State<BrewList> {
+  @override
+  Widget build(BuildContext context) {
+
+    final brews = Provider.of<List<Brew>>(context);
+    brews.forEach((brew) {
+      print(brew.name);
+      print(brew.sugars);
+      print(brew.strength);
+    });
+
+    return Container(
+
+    );
+  }
+}
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,25 +1,33 @@
+import 'package:brew_coffee/models/brew.dart';
+import 'package:brew_coffee/screens/home/brew_list.dart';
 import 'package:brew_coffee/services/auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:brew_coffee/services/database.dart';
+import 'package:provider/provider.dart';
 class Home extends StatelessWidget {
-final AuthService _auth =AuthService();
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        title: Text("Brew Coffee"),
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton.icon(
-            onPressed: () async{
-              await _auth.signOut();
-            },
-            icon: Icon(Icons.person),
-            label: Text("Logout"),
-          ),
-        ],
+    return StreamProvider<List<Brew>>.value(
+      value: DatabaseService().brews,
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text("Brew Coffee"),
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+              onPressed: () async {
+                await _auth.signOut();
+              },
+              icon: Icon(Icons.person),
+              label: Text("Logout"),
+            ),
+          ],
+        ),
+        body: BrewList(),
       ),
     );
   }
