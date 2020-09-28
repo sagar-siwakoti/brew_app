a270afff56c03f0a2e9451ac3e1f87fb1240265c
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-09-28 11:47:41 +0545

    -home page background changed and update button fixed

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -7,20 +7,19 @@ import 'package:brew_coffee/services/database.dart';
 import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
+
   final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
-    void _showSettingsPanel() {
-      showModalBottomSheet(
-          context: context,
-          builder: (context) {
-            return Container(
 
-              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
-              child: SettingsForm(),
-            );
-          });
+    void _showSettingsPanel() {
+      showModalBottomSheet(context: context, builder: (context) {
+        return Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+          child: SettingsForm(),
+        );
+      });
     }
 
     return StreamProvider<List<Brew>>.value(
@@ -28,24 +27,33 @@ class Home extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Colors.brown[50],
         appBar: AppBar(
-          title: Text("Brew Coffee"),
+          title: Text('Brew Crew'),
           backgroundColor: Colors.brown[400],
           elevation: 0.0,
           actions: <Widget>[
             FlatButton.icon(
+              icon: Icon(Icons.person),
+              label: Text('logout'),
               onPressed: () async {
                 await _auth.signOut();
               },
-              icon: Icon(Icons.person),
-              label: Text("Logout"),
             ),
             FlatButton.icon(
-                onPressed: () => _showSettingsPanel(),
-                icon: Icon(Icons.settings),
-                label: Text("Settings")),
+              icon: Icon(Icons.settings),
+              label: Text('settings'),
+              onPressed: () => _showSettingsPanel(),
+            )
           ],
         ),
-        body: BrewList(),
+        body: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/coffee_bg.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: BrewList()
+        ),
       ),
     );
   }
